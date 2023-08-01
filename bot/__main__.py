cecf6afd58316184b1c767ca47a5e80b88bc4450
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-01 10:20:20 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -202,7 +202,7 @@ async def start(_, message):
         msg = 'Token refreshed successfully!\n\n'
         msg += f'Validity: {get_readable_time(int(config_dict["TOKEN_TIMEOUT"]))}'
         return await sendMessage(message, msg)
-    elif await CustomFilters.authorized(client, message):
+    elif await CustomFilters.authorized(_, message):
         start_string = await sendMessage(message, start_string, reply_markup, 'This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.', help_command=f"/{BotCommands.HelpCommand}")
     elif config_dict['DM_MODE']:
         await sendMessage(message, 'Now, This bot will send all your files and links here. Start Using ...', reply_markup)
