e586614d835106ba55fa131bc18c7e06648e7238
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-01 20:51:11 -0700

    Create wzml_minimal.py

---
+++ bot/helper/themes/wzml_minimal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+class WZMLStyle:
+    # async def start(client, message) ---> __main__.py
+    ST_BN1_NAME = 'Repo'
+    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
+    ST_BN2_NAME = 'Updates'
+    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+<b>Type {help_command} to get a list of available commands</b>'''
+    ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
+    # ---------------------
