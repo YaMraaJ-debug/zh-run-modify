0c9b6471b57937a3a0c0e2d8f543293bb2508902
SivaSoft <58784617+squintyt@users.noreply.github.com>
2023-08-02 19:41:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -211,7 +211,7 @@ async def start(client, message):
         return await sendMessage(message, msg, reply_markup)
     elif await CustomFilters.authorized(client, message):
         start_string = BotTheme('ST_MSG', help_command=f"/{BotCommands.HelpCommand}")
-        await sendMessage(message, start_string, reply_markup photo='https://graph.org/Qtvs-leech-Bot-Arguments-08-02') )
+        await sendMessage(message, start_string, reply_markup, photo='https://graph.org/Qtvs-leech-Bot-Arguments-08-02') )
     elif config_dict['DM_MODE']:
         await sendMessage(message, BotTheme('ST_BOTPM'), reply_markup, photo='https://graph.org/Qtvs-leech-Bot-Arguments-08-02')
     else:
