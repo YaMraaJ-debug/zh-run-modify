36276a1b7e379b2d061934ea21a7dba2cef95ff0
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-03 09:03:00 -0700

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -67,9 +67,9 @@ async def broadcast(_, message):
                 u += 1
             t += 1
         return await editMessage(temp_wait, f'''⌬  <b><i>Broadcast Deleted Stats :</i></b>
-┠ <b>Total Users:</b> <code>{t}</code>
-┠ <b>Success:</b> <code>{s}</code>
-┖ <b>Unsuccess Attempt:</b> <code>{u}</code>
+╭<b>Total Users:</b> <code>{t}</code>
+├<b>Success:</b> <code>{s}</code>
+╰<b>Unsuccess Attempt:</b> <code>{u}</code>
 
 <b>Broadcast ID:</b> <code>{bc_id}</code>''')
     elif edited:
@@ -88,18 +88,18 @@ async def broadcast(_, message):
                 u += 1
             t += 1
         return await editMessage(temp_wait, f'''⌬  <b><i>Broadcast Edited Stats :</i></b>
-┠ <b>Total Users:</b> <code>{t}</code>
-┠ <b>Success:</b> <code>{s}</code>
-┖ <b>Unsuccess Attempt:</b> <code>{u}</code>
+╭<b>Total Users:</b> <code>{t}</code>
+├<b>Success:</b> <code>{s}</code>
+╰<b>Unsuccess Attempt:</b> <code>{u}</code>
 
 <b>Broadcast ID:</b> <code>{bc_id}</code>''')
     start_time = time()
     status = '''⌬  <b><i>Broadcast Stats :</i></b>
-┠ <b>Total Users:</b> <code>{t}</code>
-┠ <b>Success:</b> <code>{s}</code>
-┠ <b>Blocked Users:</b> <code>{b}</code>
-┠ <b>Deleted Accounts:</b> <code>{d}</code>
-┖ <b>Unsuccess Attempt:</b> <code>{u}</code>'''
+╭<b>Total Users:</b> <code>{t}</code>
+├<b>Success:</b> <code>{s}</code>
+├<b>Blocked Users:</b> <code>{b}</code>
+├<b>Deleted Accounts:</b> <code>{d}</code>
+╰<b>Unsuccess Attempt:</b> <code>{u}</code>'''
     updater = time()
     bc_hash, bc_msgs = str(uuid4()), []
     pls_wait = await sendMessage(message, status.format(**locals()))
