d56329a8e6e55decf94fa0b98e81900c3df01a63
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-03 00:45:27 -0700

    Create skull.py

---
+++ bot/helper/themes/skull.py
@@ -0,0 +1,10 @@
+
+class SKULL:
+    ST_BN1_NAME = 'Repo'
+    ST_BN1_URL = 'https://github.com/BalaPriyan/zh-run'
+    ST_BN2_NAME = 'Updates'
+    ST_BN2_URL = 'https://t.me/KingOfFondness'
+    ST_MSG = '''<i>This bot is running in modified repo to get the repo click on repo but give credits</i>
+<b>Type {help_command} to get a list of available commands</b>'''
+    ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own Mirror-Leech bot</i>'''
