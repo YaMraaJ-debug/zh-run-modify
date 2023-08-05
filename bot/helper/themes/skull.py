94de3f4127c9b046905755483e94fccd80e18b51
Yamraaj Yaduvanshi <69627019+YaMraaJ-debug@users.noreply.github.com>
2023-08-05 16:00:27 +0530

    Update skull.py

--- bot/helper/themes/skull.py
+++ bot/helper/themes/skull.py
@@ -2,11 +2,11 @@ from bot import config_dict
 
 class SKULL:
     ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://github.com/BalaPriyan/zh-run'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/KingOfFondness'
+    ST_BN1_URL = 'https://github.com/lzzy12/python-aria-mirror-bot'
+    ST_BN2_NAME = 'Modify by'
+    ST_BN2_URL = 'https://t.me/YaMraaJ_xD'
     ST_MSG = '''<i>This bot is running in modified repo to get the repo click on repo but give credits</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own Mirror-Leech bot</i>'''
-    PIC = 'https://graph.org/file/d454b953103d42d759f8d.jpg'
+    PIC = 'https://telegra.ph/YaMraaJ-logo-08-05'
